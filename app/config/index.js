064def47ed8f12d76b8a3c1f95175b60148ef7a9
Mike  Boice <mkboice@gmail.com>
2016-03-06 14:33:42 -0700

    Add sessions and passport support

---
+++ app/config/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+if(process.env.NODE_ENV === 'production') {
+	//Offer production starge environment variables
+	module.exports = {
+		host: process.env.host || "",
+		dbURI: process.env.dbURI,
+		sessionSecret: process.env.sessionSecret,
+		"fb": {
+			"clientID": process.env..fbClientID,
+			"clientSecret": process.env.fbClientSecret,
+			"callbackURL": process.env.host + "/auth/facebook/callback",
+			"profileFields":["id", "displayName", "photos"]
+	}
+	}
+} else {
+	//Offer dev stage settings and data
+	module.exports = require('./development.json');
+}
