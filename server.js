ab6ad8093e4e3212ccbc255c57559544bc7e4b93
Mike  Boice <mkboice@gmail.com>
2016-03-05 19:50:18 -0700

    Handle routing

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 'use strict';
 const express = require('express');
 const app = express();
+const chatCat = require('./app');
+
 app.set('port', process.env.PORT ||3000);
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-let helloMiddleware = (req, res, next) => {
-    req.hello = "Hello! It's me! I was wondering .... you get the idea!";
-    next();
-}
-
-app.use(helloMiddleware);
-
-app.get('/', (req, res, next) => {
-    res.send('<h1>Hello Express!</h1>');
-});
-
-app.get('/dashboard', (req, res, next) => {
-    res.send('<h1>This is the dashboard page! Middleware says: ' + req.hello + '</h1>');
-})
+app.use('/', chatCat.router);
 
 app.listen(app.get('port'), ()=> {
     console.log('ChatCAT running on Port: ' + app.get('port'))
-})
+});
